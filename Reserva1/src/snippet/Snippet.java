173478cccc086860f9a9771c2010baaab408dff8
Gabriel Daniel Gaburi <89953242+GabrielGaburi@users.noreply.github.com>
2023-11-04 21:53:16 -0300

    Add files via upload

---
+++ Reserva1/src/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		System.out.println("Erro na reserva, a data do Check-out é inferior a data do Check-In");
+	}
+}
+
